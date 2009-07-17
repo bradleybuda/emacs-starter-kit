0e07cbeb106803019558910721fac12053f74ff2
Bradley Buda <brad@tracksimple.com>
2009-07-17 00:01:16 -0700

    jabber params (all public)

---
+++ brad/jabber-init.el
@@ -0,0 +1,10 @@
+(require 'jabber)
+
+(custom-set-variables
+ '(jabber-connection-type (quote ssl))
+ '(jabber-network-server "talk.google.com")
+ '(jabber-port 5223)
+ '(jabber-server "tracksimple.com")
+ '(jabber-username "brad")
+ '(jabber-nickname "Brad") 
+ )
