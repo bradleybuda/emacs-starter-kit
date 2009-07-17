0473273c822f4739c3554629727d1dd27925423f
Bradley Buda <brad@tracksimple.com>
2009-07-17 10:27:38 -0700

    Going with the 'blackboard' color theme for now

--- brad/textmate-colors.el
+++ brad/textmate-colors.el
@@ -1,29 +1,34 @@
-;; Set color scheme (set lconfig-dark-bg-scheme to t for reverse color scheme)
-(defconst color-scheme 'dark)
-(defconst foreground-color "gray85")
-(defconst background-color "gray25")
-(defconst cursor-color "red3")
-(defconst pointer-color "white")
 
-(if (featurep 'xemacs)
-    (let ((frame (selected-frame)))
-      (set-face-foreground 'default foreground-color)
-      (set-face-background 'default background-color)
-      (setq frame-background-mode color-scheme)
-      color-scheme
-      (set-frame-property frame
-                          'custom-properties
-                          (mapcar (lambda (symbol)
-                                    (if (eql symbol 'light)
-                                        'dark
-                                      symbol))
-                                  (frame-property frame
-                                                  'custom-properties))))
-  (progn 
-     (add-to-list 'default-frame-alist '(foreground-color . "gray85"))
-     (add-to-list 'default-frame-alist '(background-color . "gray25"))
-     (add-to-list 'default-frame-alist '(cursor-color . "red3"))
-     (add-to-list 'default-frame-alist '(background-mode . dark))
-     (set-cursor-color cursor-color)
-     (set-mouse-color pointer-color))
-)
+;; Pieter's colors
+;; disabled for now
+;; Set color scheme (set lconfig-dark-bg-scheme to t for reverse color scheme)
+;; (defconst color-scheme 'dark)
+;; (defconst foreground-color "gray85")
+;; (defconst background-color "gray25")
+;; (defconst cursor-color "red3")
+;; (defconst pointer-color "white")
+
+;; (if (featurep 'xemacs)
+;;     (let ((frame (selected-frame)))
+;;       (set-face-foreground 'default foreground-color)
+;;       (set-face-background 'default background-color)
+;;       (setq frame-background-mode color-scheme)
+;;       color-scheme
+;;       (set-frame-property frame
+;;                           'custom-properties
+;;                           (mapcar (lambda (symbol)
+;;                                     (if (eql symbol 'light)
+;;                                         'dark
+;;                                       symbol))
+;;                                   (frame-property frame
+;;                                                   'custom-properties))))
+;;   (progn 
+;;      (add-to-list 'default-frame-alist '(foreground-color . "gray85"))
+;;      (add-to-list 'default-frame-alist '(background-color . "gray25"))
+;;      (add-to-list 'default-frame-alist '(cursor-color . "red3"))
+;;      (add-to-list 'default-frame-alist '(background-mode . dark))
+;;      (set-cursor-color cursor-color)
+;;      (set-mouse-color pointer-color))
+;; )
+
+(color-theme-blackboard)
