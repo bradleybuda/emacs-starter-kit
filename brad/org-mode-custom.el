f815e697647f5f10e0bf0082f3ba976cfcc3ed84
Bradley Buda <brad@tracksimple.com>
2009-07-17 10:43:18 -0700

    add in my org mode colors and workflow

---
+++ brad/org-mode-custom.el
@@ -0,0 +1,11 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(org-todo-keyword-faces (quote (("DONE" :foreground "green" :weight bold)
+                                  ("BLOCKED" :foreground "red" :weight bold)
+                                  ("IN_PROGRESS" :foreground "purple" :weight bold)
+                                  ("TODO" :foreground "white" :weight bold))))
+ '(org-todo-keywords (quote ("TODO" "IN_PROGRESS" "BLOCKED" "DONE")))
+)
